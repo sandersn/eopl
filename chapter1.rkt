a8a2346a0afcbf66c2727ad272c77c5796a228bc
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2022-06-20 17:19:26 -0700

    Switch to typed/racket and use ADTs
    ADTs are great except they're from some random package that hasn't been
    updated in 7 years.

--- chapter1.rkt
+++ chapter1.rkt
@@ -1,4 +1,5 @@
-#lang racket
+#lang typed/racket
+(require datatype)
 ; (define-struct node (kind start end kids))
 ; (define tree
 ;   (node "SourceFile" 0 20
@@ -26,21 +27,28 @@
 ; (display (node-kind (node-at-location tree 6)))
 ; (newline)
 ; (display (node-kind (node-at-location tree 16)))
-(define/match (occurs-free? tree x)
-    [(`(lambda (,ps ...) ,e) x) (and (not (memq x ps)) (occurs-free? e x))]
-    [(`(if ,cond ,then ,else) x) (if (memf (λ (e) (occurs-free? e x)) (list cond then else)) #t #f)]
-    [((list e1 e2) x) (or (occurs-free? e1 x) (occurs-free? e2 x))]
-    [(y y) #t]
-    [(y x) #f])
-(define/match (occurs-bound? tree x)
-  [(`(lambda (,ps ...) ,e) x) 
-   (or (occurs-bound? e x) 
-       (match (memq x ps)
-         [(cons y _) (and (eq? x y) (occurs-free? y e))]
-         [_ #f]))]
-  [((list e1 e2) x) (or (occurs-bound? e1 x) (occurs-bound? e2 x))]
-  [(y x) #f])
-
+(define-datatype Exp
+  [Var (Symbol)]
+  [Lambda ((Listof Symbol) Exp)]
+  [If (Exp Exp Exp)]
+  [App (Exp Exp)])
+(: occurs-free? (-> Exp Symbol Boolean))
+(define (occurs-free? tree x)
+  (type-case Exp tree
+             [(Var v) => (eq? v x)]
+             [(Lambda ps e) => (and (not (memq x ps)) (occurs-free? e x))]
+             [(If cond then else) => (if (memf (lambda ([e : Exp]) (occurs-free? e x)) (list cond then else)) #t #f)]
+             [(App e1 e2) => (or (occurs-free? e1 x) (occurs-free? e2 x))]))
+(: occurs-bound? (-> Exp Symbol Boolean))
+(define (occurs-bound? tree x)
+  (type-case Exp tree
+             [(Var _) => #f]
+             [(Lambda ps e) => (or (occurs-bound? e x) (match (memq x ps)
+                                                         [(cons y _) (and (eq? x y) (occurs-free? e y))]
+                                                         [_ #f]))]
+             [(If cond then else) => (if (memf (lambda ([e : Exp]) (occurs-bound? e x)) (list cond then else)) #t #f)]
+             [(App e1 e2) => (or (occurs-bound? e1 x) (occurs-bound? e2 x))]))
+(: test (All (T U ...) (-> (-> U ... U T) (-> T U ... U Void))))
 (define (test f)
   (λ (expect . xs)
     (display xs)
@@ -49,21 +57,23 @@
           (display " ok")
           (display " fail")))
     (newline)))
+(: tof? (-> Boolean Exp Symbol Void))
 (define tof? (test occurs-free?))
+(: tob? (-> Boolean Exp Symbol Void))
 (define tob? (test occurs-bound?))
-(tof? #f '(lambda (x) x) 'x)
-(tof? #f '(lambda (x a b) x) 'x)
-(tof? #t '(lambda (y) x) 'x)
-(tof? #t '(lambda (y a b) (if a x y)) 'x)
-(tof? #f '(lambda (y) x) 'y)
-(tof? #f '(lambda (y a b) x) 'y)
-(tof? #t 'x 'x)
-(tof? #f 'y 'x)
-(tob? #t '(lambda (x) x) 'x)
-(tob? #t '(lambda (x a b) x) 'x)
-(tob? #f '(lambda (y) x) 'x)
-(tob? #f '(lambda (y a b) x) 'x)
-(tob? #f '(lambda (y) x) 'y)
-(tob? #f '(lambda (y a b) x) 'y)
-(tob? #f 'y 'y)
-(tob? #f 'y 'x)
+(tof? #f (Lambda '(x) (Var 'x)) 'x)
+(tof? #f (Lambda '(x a b) (Var 'x)) 'x)
+(tof? #t (Lambda '(y) (Var 'x)) 'x)
+(tof? #t (Lambda '(y a b) (If (Var 'a) (Var 'x) (Var 'y))) 'x)
+(tof? #f (Lambda '(y) (Var 'x)) 'y)
+(tof? #f (Lambda '(y a b) (Var 'x)) 'y)
+(tof? #t (Var 'x) 'x)
+(tof? #f (Var 'y) 'x)
+(tob? #t (Lambda '(x) (Var 'x)) 'x)
+(tob? #t (Lambda '(x a b) (Var 'x)) 'x)
+(tob? #f (Lambda '(y) (Var 'x)) 'x)
+(tob? #f (Lambda '(y a b) (Var 'x)) 'x)
+(tob? #f (Lambda '(y) (Var 'x)) 'y)
+(tob? #f (Lambda '(y a b) (Var 'x)) 'y)
+(tob? #f (Var 'y) 'y)
+(tob? #f (Var 'y) 'x)
